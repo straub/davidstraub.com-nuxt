7baa16e042a4b25abdf0b107aea3c09b4b029fb5
David Straub <scinvention@gmail.com>
2024-02-09 20:29:25 -0500

    chore(deps): rm vue-axe and axe-core

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,6 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    { src: '~/plugins/axe.js', mode: 'client' },
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
